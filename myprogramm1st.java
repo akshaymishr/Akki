517f786ce87c994415caa087c195dbb5dd4eab18
akshaymishr <akshaymishra6767@gmail.com>
2024-03-13 13:04:36 +0530

    myfirstcommit

---
+++ myprogramm1st.java
@@ -0,0 +1,13 @@
+package basicsofjava;//print your name
+
+public class Assignment1_printyou_rName // identifier
+
+{
+	// keywords
+	public static void main(String[] args) //main mothed // comments
+   {                                      
+	System.out.println("Prerit Mishra");//literals
+   }	
+	
+} //separator
+
